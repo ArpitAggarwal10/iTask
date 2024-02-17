c1cacb527673d1836f3957ef00a6d5378e8034a1
Arpit Aggarwal <arpitaggarwal1012@gmail.com>
2024-02-18 02:53:07 +0530

    Initial Commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='flex justify-around bg-indigo-900 text-white py-2 mt-10'>
+        <div className="">
+            <span className='text-sm'>Developed By Arpit Aggarwal</span>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
